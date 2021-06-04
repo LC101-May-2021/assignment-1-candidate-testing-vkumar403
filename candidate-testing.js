d9e042cd53ad86c75b76a5bec4bd621c3a3f6969
vkumar403 <vanitha.vellingiri@gmail.com>
2021-06-04 16:46:04 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,13 +11,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = [
-"True or false: 5 kilometer == 5000 meters? ",
-"(5 + 3)/2 * 10 = ? ",
-"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-"Who was the first American woman in space? ",
-"What is the minimum crew size for the ISS? "
-];
+let questions = ["True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","Who was the first American woman in space? ","What is the minimum crew size for the ISS? "];
 
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
