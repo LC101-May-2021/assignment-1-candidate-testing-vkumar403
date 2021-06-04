95df1a7bae0f007684f5542949e7e0f7aba2cd78
vkumar403 <vanitha.vellingiri@gmail.com>
2021-06-04 16:43:40 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -14,7 +14,7 @@ let candidateAnswer = "";
 let questions = [
 "True or false: 5 kilometer == 5000 meters? ",
 "(5 + 3)/2 * 10 = ? ",
-'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
 "Who was the first American woman in space? ",
 "What is the minimum crew size for the ISS? "
 ];
